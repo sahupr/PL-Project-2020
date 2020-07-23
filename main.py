81eaabde80e8202f0828860f9cb33505df0e1130
Quan Do <minhquand3@gmail.com>
2020-07-23 17:19:40 -0400

    Changed os to subprocess

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 import json
-import os
+import subprocess
 
 
 class Task:
-    def __init__(self, name, num):
+    def __init__(self, name, cmd):
         self.name = name
-        self.num = num
+        self.cmd = cmd
 
     def run(self, *inps):
-        print('\n', self.name)
-        for inp in inps:
-            with open(inp, 'r') as f:
-                if 'json' in inp:
-                    data = json.load(f)
-                else:
-                    data = f.readlines()
-            print(data)
-        if self.num == 1:
-            # Insert command code to run task 1
-            os.system('echo Task1')
-        elif self.num == 2:
-            # Insert command code to run task 2
-            os.system('echo Task2')
-        elif self.num == 3:
-            # Insert command code to run task 3
-            os.system('echo Task3')
-        elif self.num == 4:
-            # Insert command code to run task 4
-            os.system('echo Task4')
+        # print('\n', self.name)
+        # for inp in inps:
+        #     with open(inp, 'r') as f:
+        #         if 'json' in inp:
+        #             data = json.load(f)
+        #         else:
+        #             data = f.readlines()
+        #     print(data)
+        output = subprocess.run([self.cmd, self.name], shell=True,
+                                capture_output=True, encoding='utf_8').stdout
+        print(output)
 
 
-task1 = Task('Task1', 1)
-task2 = Task('Task2', 2)
-task3 = Task('Task3', 3)
-task4 = Task('Task4', 4)
+task1 = Task('Task1', 'echo')
+task2 = Task('Task2', 'echo')
+task3 = Task('Task3', 'echo')
+task4 = Task('Task4', 'echo')
 
 task1.run('input1.json', 'input1.txt')
 task2.run('output1.json', 'input2.txt')
