aef6b5d0a69565a53e6175d19b4de5f606052331
Quan Do <minhquand3@gmail.com>
2020-07-29 19:47:42 -0400

    Change image resolution to fixed size

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def draw():
             elif matrix[i][j] == 2:
                 color = (0, 0, 255)
             pixels[j, i] = color
-    image = image.resize((width * 100, height * 100), resample=Image.BOX)
+    image = image.resize((10000, 10000), resample=Image.BOX)
     image.save('./output.png')
 
 
