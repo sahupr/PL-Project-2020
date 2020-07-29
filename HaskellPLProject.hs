f62c14ce8632da3b4f7a728d78fa5fc2e87f6ef5
ufferm <68293011+ufferm@users.noreply.github.com>
2020-07-28 21:05:18 -0400

    Add files via upload

--- HaskellPLProject.hs
+++ HaskellPLProject.hs
@@ -1,21 +1,23 @@
---max3 :: (Double, Double, Double) -> Double
---max3 (x, y, z) = max (max x y) z
-traverseTemp :: ([Integer], Integer, Integer, Integer) -> [Integer]
-traverseTemp (temp, x, y, i) = 
-	if i == y
-		then num = temp !! pathY
-	
 
-matrixPath :: ([[Integer]], [[Integer]], Integer, Integer, [[Integer]]) -> [[Integer]])
+replace pos newVal list = take pos list ++ newVal : drop (pos+1) list
+
+matrixPath :: ([[Int]], [[Int]], Int, Int, [[Int]]) -> [[Int]]
+matrixPath (matrix, path, i, ix, [[]]) = 
+    if i == length path
+	    then [[]]
+	else
+        if (path !! ix !! 0) == i	
+            then matrixPath(matrix, path, i+1, ix + 1, ([replace (path !! ix !! 1) 2 (matrix !! i)]))
+        else
+			matrixPath (matrix, path, i+1, ix, ([matrix !! i]))
 matrixPath (matrix, path, i, ix, output) = 
-	temp = matrix !! i
-	pathX = path !! ix !! 0
-	pathY = path !! ix !! 1
-	if pathX == i
-		then temp = traverseTemp (temp, pathX, pathY, 0)
-			output = output ++ temp
-	if i > length path
-		then print output
-	else matrixPath (matrix, path, i+1, ix+1, output)
-		
-	
+	if ix == length path
+	    then if i == length matrix
+		        then output
+			else 
+			    matrixPath(matrix,path,i+1,ix,(output ++ [matrix !! i]))
+	else
+        if (path !! ix !! 0) == i	
+            then matrixPath(matrix, path, i+1, ix + 1, (output ++ [replace (path !! ix !! 1) 2 (matrix !! i)]))
+        else
+			matrixPath (matrix, path, i+1, ix, (output ++ [matrix !! i]))
